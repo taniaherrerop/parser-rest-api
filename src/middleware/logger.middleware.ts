6076e99e12e09d73a1dd2a6e89ecbda9354482ee
taniaherrerop <60565745+taniaherrerop@users.noreply.github.com>
2020-02-02 15:00:28 -0500

    Delete logger.middleware.ts

--- src/middleware/logger.middleware.ts
+++
@@ -1,8 +0,0 @@
-import * as express from 'express';
-
-function loggerMiddleware(request: express.Request, response: express.Response, next) {
-  console.log(`${request.method} ${request.path}`);
-  next();
-}
-
-export default loggerMiddleware;
